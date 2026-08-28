70fb22e7ed1600d8e75d191991a5df0bf2f8738b
Suryansh Tamrakar <tamrakarsuryansh89@gmail.com>
2026-08-28 14:35:38 +0530

    Update Gemini model

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -4,7 +4,7 @@ const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_GEMINI_KEY });
 
 async function generateAIResponse(prompt) {
     const response = await ai.models.generateContent({
-      model: 'gemini-3.1-flash-lite',
+      model: 'gemini-3.5-flash-lite',
         contents: prompt,
     });
     return response.text;
